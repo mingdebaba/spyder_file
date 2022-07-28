37c52cf8e15effd301ab7201e65056307e86e853
mingdebaba <51104897+mingdebaba@users.noreply.github.com>
2022-07-28 21:36:33 +0900

    演算

---
+++ xor2.py
@@ -0,0 +1,13 @@
+from sklearn.neighbors import  KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+learn_data = [[0,0],[1,0],[0,1],[1,1]]
+learn_label=[0,1,1,0]
+clf = KNeighborsClassifier(n_neighbors=1)
+
+clf.fit(learn_data,learn_label)
+test_data = [[0,0],[1,0],[0,1],[1,1]]
+test_label = clf.predict(test_data)
+
+print(test_data,"predict result:" ,test_label)
+print(" accurate  = ",accuracy_score([0,1,1,0],test_label))
